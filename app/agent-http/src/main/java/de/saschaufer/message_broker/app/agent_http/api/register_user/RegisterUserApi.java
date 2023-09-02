fae98c95435d2fbacc8de0f057d488c9bb26ef75
sascha <sufer@posteo.de>
2023-09-03 01:56:48 +0200

    Fixes a bug where the request body is not validated in the RegisterUserApi.

--- app/agent-http/src/main/java/de/saschaufer/message_broker/app/agent_http/api/register_user/RegisterUserApi.java
+++ app/agent-http/src/main/java/de/saschaufer/message_broker/app/agent_http/api/register_user/RegisterUserApi.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -56,6 +57,6 @@ public interface RegisterUserApi {
                     required = false
             ) final String correlationId,
 
-            @RequestBody final User user
+            @RequestBody @Validated final User user
     ) throws ServiceUnavailableException;
 }
